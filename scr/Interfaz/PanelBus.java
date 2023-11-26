91b347521273bc4ea8a3d61afda522d05d4e9680
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-11-26 17:51:48 -0300

    botones

--- scr/Interfaz/PanelBus.java
+++ scr/Interfaz/PanelBus.java
@@ -6,13 +6,12 @@ public class PanelBus extends JPanel {
     private JMenu SeleccionHorarios;
     private JMenuBar BarraHorarios;
     public PanelBus(){
-        this.setLayout(null);
         ButtonGroup grupo = new ButtonGroup();
 
         JToggleButton busUnPiso = new JToggleButton("Bus de 1 piso");
-        busUnPiso.setBounds(100,300,100,100);
+        busUnPiso.setBounds(100,300,110,50);
         JToggleButton busDosPisos = new JToggleButton("Bus de 2 pisos");
-        busDosPisos.setBounds(100,200,100,100);
+        busDosPisos.setBounds(100,200,110,50);
 
         grupo.add(busUnPiso);
         grupo.add(busDosPisos);
@@ -20,17 +19,17 @@ public class PanelBus extends JPanel {
         this.add(busUnPiso);
         this.add(busDosPisos);
 
-        BarraHorarios opcion1 = new BarraHorarios("10:00 Am");
-        BarraHorarios opcion2 = new BarraHorarios("15:00 Pm");
-        BarraHorarios opcion3 = new BarraHorarios("22:00 Pm");
+        BarraHorarios opc1 = new BarraHorarios("10:00 Am");
+        BarraHorarios opc2 = new BarraHorarios("15:00 Pm");
+        BarraHorarios opc3 = new BarraHorarios("22:00 Pm");
 
         BarraHorarios = new JMenuBar();
-        BarraHorarios.setBounds(100,425,100,50);
+        BarraHorarios.setBounds(100,425,150,100);
         this.add(BarraHorarios);
-        SeleccionHorarios = new JMenu("Origen");
-        SeleccionHorarios.add(opcion1.getItem());
-        SeleccionHorarios.add(opcion2.getItem());
-        SeleccionHorarios.add(opcion3.getItem());
+        SeleccionHorarios = new JMenu("Horarios");
+        SeleccionHorarios.add(opc1.getItem());
+        SeleccionHorarios.add(opc2.getItem());
+        SeleccionHorarios.add(opc3.getItem());
         BarraHorarios.add(SeleccionHorarios);
 
 
