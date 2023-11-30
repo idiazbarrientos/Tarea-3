f379d62f0e14fdc24415e19d792a5fe68eeb868d
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-11-30 10:56:23 -0300

    Asiento un poco de logica

--- scr/Codigo/Asiento.java
+++ scr/Codigo/Asiento.java
@@ -3,9 +3,16 @@ package Codigo;
 abstract public class Asiento {
     private int precio;
     private int numeroAsiento;
+    private TipoAsiento type;
 
-    public Asiento(int numeroAsiento) {
+    public Asiento(int numeroAsiento, TipoAsiento type) {
         this.numeroAsiento = numeroAsiento;
+    }
+    public int getNumeroAsiento(){
+        return numeroAsiento;
+    }
+    public TipoAsiento getType(){
+        return type;
     }
     public abstract String TipoAsiento();
 }
