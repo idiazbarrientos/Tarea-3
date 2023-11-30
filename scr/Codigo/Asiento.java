b8c4ceee25a79002c8d956c4faa7b7aee7a28f8a
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-11-30 11:53:19 -0300

    Atributos Asiento

--- scr/Codigo/Asiento.java
+++ scr/Codigo/Asiento.java
@@ -5,15 +5,39 @@ abstract public class Asiento {
     private int precio;
     private int numeroAsiento;
     private TipoAsiento type;
+    private boolean select;
+    private boolean comprado;
 
     public Asiento(int numeroAsiento, TipoAsiento type) {
         this.numeroAsiento = numeroAsiento;
     }
-    public int getNumeroAsiento(){
+
+    public int getNumeroAsiento() {
         return numeroAsiento;
     }
-    public TipoAsiento getType(){
+
+    public TipoAsiento getType() {
         return type;
     }
+
     public abstract String TipoAsiento();
+
+    public void select() {
+        if (!comprado) {
+            select = true;
+        }
+    }
+    public void deselected(){
+        select = false;
+    }
+    void comprar(){
+        this.comprado = true;
+        this.select = false;
+    }
+    public boolean isSelect(){
+        return select;
+    }
+    public boolean isComprado(){
+        return comprado;
+    }
 }
--- scr/Interfaz/ValorPago.java
+++ scr/Interfaz/ValorPago.java
@@ -1,6 +1,12 @@
 package Interfaz;
 
+import Codigo.Viaje;
+
 import javax.swing.*;
 
 public class ValorPago extends JPanel {
+    public Viaje viaje;
+    private ValorPago(){
+
+    }
 }
