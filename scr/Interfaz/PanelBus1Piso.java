7f62d106bcdcb4e5fb8ab5468102de63b7993949
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-12-08 23:21:21 -0300

    Update PanelBus1Piso.java

--- scr/Interfaz/PanelBus1Piso.java
+++ scr/Interfaz/PanelBus1Piso.java
@@ -12,15 +12,13 @@ public class PanelBus1Piso extends JPanel {
         public PanelBus1Piso(){
             setLayout(new GridLayout(10, 0, 5, 5));
             int TotalAsientos = 14;
-            ArrayList<JButton> array_botones = new ArrayList<>();
+            ArrayList<BotonAsientos> array_botones = new ArrayList<>();
 
             for(int i=1; i<=TotalAsientos; i++){
                 array_botones.add(new JButton(" "+i));
             }
             for(int i = 1; i <= TotalAsientos; i++) {
-                //JButton btnAsiento = new JButton(" " + i);
                 array_botones.get(i-1).setSize(50,50);
-               // btnAsiento.setSize(50, 50);
                 array_botones.get(i-1).addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
