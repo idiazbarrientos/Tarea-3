ba44d499da8bdd116a9565a0efe6505438f23672
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-12-10 14:46:11 -0300

    Panel que mostrara el precio a pagar
    por completar con los precios

--- scr/Interfaz/ValorPago.java
+++ scr/Interfaz/ValorPago.java
@@ -5,8 +5,18 @@ import Codigo.Viaje;
 import javax.swing.*;
 
 public class ValorPago extends JPanel {
-    public Viaje viaje;
+    private static ValorPago instance;
     private ValorPago(){
 
     }
+    public static ValorPago getInstance(){
+        if(instance == null){
+            instance = new ValorPago();
+        }
+        return instance;
+    }
+    public void updatePrecio(){
+        JLabel total = new JLabel("Precio Total : $"+ //aqui poner el valor de precio de los asientos)
+    }
+
 }
